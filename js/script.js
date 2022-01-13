52964f348f1aceac597d7c4a6b231e9b15587e0c
dimisyko <dimitri.syko@hotmail.com>
2022-01-13 15:38:14 +0100

    brussels design

--- js/script.js
+++ js/script.js
@@ -9,11 +9,10 @@ let animHome = gsap.timeline({
 })
 animHome.to('.imgClip', {
     clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)"
-}, "a")
-animHome.from('h1', {
-    opacity : 1
-}, "a")
-
+})
+animHome.from('h1, .scroll-down', {
+    opacity : 1,
+}, "0.2")
 /*Agrandissement image*/
 let scaleImg = gsap.timeline({
     scrollTrigger :{
@@ -27,20 +26,28 @@ scaleImg.to('.image-scale', {
     width : '100vw',
     height : '100vh',
 })
-
+/*Transition fond noir*/
+let bgTransi = gsap.timeline({
+    scrollTrigger :{
+        trigger : '.bg-fixed',
+        start : 'top top',
+        pin : true,
+        scrub : true
+    }
+})
+bgTransi.to('.bg-transi', {
+    height : '100%',
+})
 /*Curseur*/
 function cursor(e){
-
-    var cursor = document.querySelector('.cursor')
-    var x = e.pageX - 15
-    var y = e.pageY - window.pageYOffset - 15
-
+    var curseur = document.querySelector('.cursor')
+    var x = e.pageX - 10
+    var y = (e.pageY - window.pageYOffset) - 10
     setTimeout(function() {
-    cursor.style.transform = 'translate3d('+x+'px, '+y+'px, 0px)'
+    curseur.style.transform = 'translate3d('+x+'px, '+y+'px, 0px)'
 }, 80);
 }
 document.addEventListener('mousemove', cursor)
-
 /*defilement Axe X image*/
 function animImg(){
     var listImg = document.querySelector('.list-img-anim')
@@ -56,27 +63,24 @@ listImg.style.transform = 'translate3d('+stopAnim+'px, 0px, 0px)'
 }
 }
 window.addEventListener('scroll', animImg)
-
 /*Change couleur background au survol des expo*/
 var img = document.querySelectorAll('.container-expo .image')
 img.forEach(function(imgs){
     imgs.addEventListener('mousemove', function(){
-        document.body.style.backgroundColor = imgs.getAttribute('data-bg')
+        document.querySelector('.bg-gray').style.backgroundColor = imgs.getAttribute('data-bg')
     })
     imgs.addEventListener('mouseleave', function(){
-        document.body.style.backgroundColor = 'white'
+        document.querySelector('.bg-gray').style.backgroundColor = '#525252'
     })
 })
-
 /*Scroll horizontale*/
 let sect = gsap.utils.toArray('.container-expo')
-var expo = document.querySelector('.current-exposition')
+var expo = document.querySelector('.actuelle-exposition')
 if(expo){
 gsap.to(expo,{
-
 xPercent : -100 * (sect.length - 1),
     scrollTrigger :{
-        trigger : '.current-exposition',
+        trigger : '.actuelle-exposition',
         start : 'center center',
         pin : true,
         scrub : 0.8,
@@ -94,40 +98,32 @@ gsap.from(imgsExpo,{
     }
 })
 })
-function MenuActive(){
-    var lienMenu = document.querySelectorAll('.menu a')
-    lienMenu.forEach(function(liensMenu){
-        if(liensMenu.href == window.location.href){
-            liensMenu.classList.add('active')
-        }
-    })
-}
-MenuActive()
-/* recuperer la couleur de la data pour l'appliquer sur le ::after et le titre de la page detail*/
+
+/* recuperer la couleur de la data pour l'appliquer sur le ::after*/
 function AfterColorExpo(){
     var afterExpo = document.querySelector('.intro')
-    var titreExpo = document.querySelector('.right h1')
-    if(afterExpo && titreExpo){
+    if(afterExpo){
     var data = afterExpo.getAttribute('data-color')
     afterExpo.style.setProperty('--color', data)
-    titreExpo.style.setProperty('--color', data)
 }
 }
 AfterColorExpo()
  /*transition page*/
 function tlLeave(){
-    return   gsap.to('.transi-img',{duration : 1, clipPath: "polygon(100% 0, 100% 0, 100% 100%, 100% 100%)", ease:Expo.easeInOut}),
+    return gsap.to('.transi-img',{duration : 1, clipPath: "polygon(100% 0, 100% 0, 100% 100%, 100% 100%)", ease:Expo.easeInOut}),
     gsap.to('.stagger',{duration: 0.5, stagger : 0.15, y: "-60%", opacity : 0}),
     gsap.to('.intro, .contact',{duration : 0.6, clipPath: "polygon(100% 0, 100% 0, 100% 100%, 100% 100%)", delay : 0.8}),
     gsap.to('.stagger-expo',{duration: 0.5, stagger : 0.12, y: "-100%", opacity : 0}),
-    gsap.to('.bg-title, .right p, .date, .mail, .home, .img-scale, .info-museum, .list-img, .section-exposition, .ticket, footer, .main-expo',{duration : 1, opacity : 0})
+    gsap.to('.bg-title, .right p, .date, .mail, .home, .img-scale, .info-museum, .list-img, .section-exposition, .ticket, footer, .main-expo, .arrowExpo img',{duration : 1, opacity : 0}),
+    gsap.to('.parallax .image-txt',{duration: 0.5,  opacity: 0, stagger: 0.14})
 }
 function tlInter(){
    return gsap.to('.intro, .contact',{duration : 2, clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)"}),
     gsap.to('.left .image img',{duration : 1, clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)", ease:Expo.easeInOut, delay : 0.5}),
     gsap.from('.stagger',{duration: 0.5, stagger : 0.15, y: "60%", opacity : 0,delay : 1.5}),
     gsap.to('.imgClip',{duration: 1.6, opacity:1 ,ease:Expo.easeInOut}),
-    gsap.to('.home h1',{duration: 1,  y:"0%", opacity:1, delay : 0.8})
+    gsap.to('.title',{duration: 1,  y:"0%", opacity:1, delay : 0.8, stagger: 0.4}),
+    gsap.to('.image-txt',{duration: 0.7,  clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)", stagger: 0.4, delay: 0.4})
 }
  tlInter()
 
@@ -149,54 +145,38 @@ var valeurcookie = Cookies.get('moncookie');
 if (valeurcookie != "accepte") {
     document.getElementById('cookies').style.display = 'block'
 }
-
-document.getElementById('btn').addEventListener('click', function(e){
+document.querySelector('.txt-cookies a').addEventListener('click', function(e){
     e.preventDefault()
     Cookies.set('moncookie', 'accepte', { expires: 365 });
 document.getElementById('cookies').style.display = 'none'
 })
-
+ 
 $(document).ready(function() {
 
     /* btnTop */
-    $('.flecheUp').click(function(){
+    $('.flecheUp i').click(function(){
         $("html, body").animate({scrollTop: 0}, 80);
     })
-    
-        /* Formulaire */
-    var nom = $('#nom')
-    var prenom = $('#prenom')
-    var email = $('#email')
-    var confirMail = $('#emailconfirm')
-    var symbole = /^(("[\w-+\s]+")|([\w-+]+(?:\.[\w-+]+)*)|("[\w-+\s]+")([\w-+]+(?:\.[\w-+]+)*))(@((?:[\w-+]+\.)*\w[\w-+]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][\d]\.|1[\d]{2}\.|[\d]{1,2}\.))((25[0-5]|2[0-4][\d]|1[\d]{2}|[\d]{1,2})\.){2}(25[0-5]|2[0-4][\d]|1[\d]{2}|[\d]{1,2})\]?$)/i
+  /*form*/
+        $('form').validetta({
+                onValid : function(e){
+                    e.preventDefault();
 
-    $('form').submit(function(e){
-        e.preventDefault()
+                    $.ajax({
+                        url: 'message_ajax.html',
+                        type : 'POST',
+                        data: $(this.form).serialize(),
 
-        if(nom.val() == '' || prenom.val() == '' || email.val() == '' || confirMail.val() == ''){
-            $('.erreur').text('Les champs sont vides')
+                        success: function(traitement){
 
-        }else if(email.val() != confirMail.val()){
-            $('.erreur').text('Les emails ne sont pas identiques')
-
-         } else if(!email.val().match(symbole)){
-            $('.erreur').text("La syntaxe de l'email n'est pas correct")
-            }else{   
-        $.ajax({
-        //1 definir le fichier à appeler
-        url : '#',
-        //2 methode utilisée pour envoyer les variables
-        type : 'POST',
-        success : function(){
-            $('.erreur').text('Merci pour la participation')
-
-          gsap.to('form>div,form p',{duration: 0.8, stagger: 0.2, y : '-100%', opacity:0, delay:0.5})
+                            $(".erreur").html(traitement);
+                                      gsap.to('fieldset>div,form p',{duration: 0.8, stagger: 0.2, y : '-100%', opacity:0, delay:0.5})
  
-          setTimeout(function() {
-              $('form').fadeOut()
-          }, 2000);
-        }
-     })
-    }
-    })
-});
+                            setTimeout(function() {
+                            $('form').fadeOut()
+                            }, 2000);
+                        },
+                    });
+                }
+            });
+})
