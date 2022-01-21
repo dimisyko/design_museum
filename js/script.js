3ec076d19998af711876a7987f19805160ea791d
dimisyko <dimitri.syko@hotmail.com>
2022-01-21 23:19:46 +0100

    design museum

--- js/script.js
+++ js/script.js
@@ -138,7 +138,17 @@ link.forEach(function(liens){
     }, 1400);
 })
 })
-
+/*Parallax*/
+function parallax(e){
+    const img = document.querySelectorAll('.image-txt')
+    img.forEach(function(el) {
+        var data = el.getAttribute('data-v')
+        var x = (window.innerWidth - e.clientX * data) / 100
+        var y = (window.innerHeight - e.clientY * data) / 100
+        el.style.transform = 'translate3d('+x+'px, '+y+'px, 0px)'
+    })
+}
+document.addEventListener('mousemove', parallax)
 /*cookies*/
 var valeurcookie = Cookies.get('moncookie');
 if (valeurcookie != "accepte") {
@@ -161,23 +171,37 @@ $(document).ready(function() {
         $(this).fadeOut();
         $('form').fadeIn();
     })
-  /*form*/
-        $('form').validetta({
-                onValid : function(e){
-                    e.preventDefault();
-                    $.ajax({
-                        url: 'message_ajax.html',
-                        type : 'POST',
-                        data: $(this.form).serialize(),
-
-                        success: function(message){
-                            $(".erreur").html(message);
-                            gsap.to('fieldset>div,form p',{duration: 0.8, stagger: 0.2, y : '-100%', opacity:0, delay:0.5})
-                            setTimeout(function() {
-                            $('form').fadeOut()
-                            }, 2000);
-                        },
-                    });
-                }
-            });
+        /* Formulaire */
+        var nom = $('#nom')
+        var prenom = $('#prenom')
+        var email = $('#email')
+        var confirMail = $('#emailconfirm')
+        var symbole = /^(([^<>()[\]\\.,;:#\s@"]+(\.[^<>()[\]\\.,;:#\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    
+        $('form').submit(function(e){
+            e.preventDefault()
+    
+            if(nom.val() == '' || prenom.val() == '' || email.val() == '' || confirMail.val() == ''){
+                $('.erreur').text('Les champs sont vides')
+    
+            }else if(email.val() != confirMail.val()){
+                $('.erreur').text('Les emails ne sont pas identiques')
+    
+             } else if(!email.val().match(symbole)){
+                $('.erreur').text("La syntaxe de l'email n'est pas correct")
+                }else{   
+            $.ajax({
+            url : 'message_ajax.php',
+            type : 'POST',
+            dataType : 'html',
+            success : function(message){
+                $('.erreur').html(message)
+              gsap.to('fieldset>div,form p',{duration: 0.8, stagger: 0.2, y : '-100%', opacity:0, delay:0.5})
+              setTimeout(function() {
+                  $('form').fadeOut()
+              }, 2000);
+            }
+         })
+        }
+        })
 })
