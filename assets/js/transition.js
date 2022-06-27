2f502de07322a7e25e40d1dde7392668b58c3cb3
dimisyko <dimitri.syko@hotmail.com>
2022-06-27 21:56:06 +0200

    add bg transition

--- assets/js/transition.js
+++ assets/js/transition.js
@@ -1,21 +1,36 @@
 export default function transition() {
-    tlInter()
-    function tlLeave() {
-        return gsap.to('.bg-transition', {
-            duration : 1.2,
-            ease : Power4.easeInOut,
-            transformOrigin : "bottom",
-            scaleY : 1
-        })
+    let fast = 0,
+        slow = 0,
+        stop = null,
+        run = null
+    const svg = document.querySelector('.bg')
+    const path = svg.querySelector('path')
+    function ease(start, end, duration){
+        return start + (end - start) / duration
     }
 
+    tlInter()
+    function tlLeave() {
+        fast= ease(fast, 0, 7)
+        slow= ease(slow, 0, 14)
+        path.setAttribute('d', `M 0 100 L 0 100 L 100 100 L 100 ${slow.toFixed(2)} C 50 ${fast.toFixed(2)} 50 ${fast.toFixed(2)} 0 ${slow.toFixed(2)}`)
+        svg.style.zIndex = 999
+     run = requestAnimationFrame(tlLeave)
+     cancelAnimationFrame(stop)
+    }
+ 
     function tlInter() {
-        return gsap.to('.bg-transition', {
+        const tl = gsap.timeline()
+        tl.to('.opacity', {
+            opacity : 1,
             duration : 1.2,
-            ease : Power4.easeInOut,
-            transformOrigin : "top",
-            scaleY : 0
+            delay : 0.7
         })
+        fast= ease(fast, 100, 7)
+        slow= ease(slow, 100, 14)
+        path.setAttribute('d', ` M 0 100 L 0 100 L 100 100 L 100 ${slow.toFixed(2)} C 50 ${fast.toFixed(2)} 50 ${fast.toFixed(2)} 0 ${slow.toFixed(2)}`)
+       stop = requestAnimationFrame(tlInter)
+        cancelAnimationFrame(run)
     }
     const links = document.querySelectorAll('.link')
     links.forEach(function (link) {
